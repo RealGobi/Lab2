dc08d88f98927a5bdd7179b5fd9e3a7bde87a250
Jimmy Svensson <jimmy888swe@gmail.com>
2018-10-19 11:44:22 +0200

    skrivit ut fragorna pa  websidan

---
+++ main.js
@@ -0,0 +1,46 @@
+
+
+document.addEventListener('DOMContentLoaded', function (){
+    var content = document.getElementById("content");
+    var res;
+
+
+var httpRequest = new XMLHttpRequest();   
+httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+             console.log(httpRequest.response);
+             res = httpRequest.response;
+                        
+             res.results.forEach(function(result, index ) {
+                 let info = document.createElement('div');
+                 info.classList.add('info');
+                 info.id = index;
+                 content.appendChild(info);
+                 var infoContent = ` 
+                 <div class="container">
+                  <p> ${result.question} </p>
+                </div> `;
+                document.getElementById(info.id).innerHTML = infoContent; 
+             });
+
+
+
+
+
+
+        } else  {
+                console.log('Något är fel, gör rätt!');
+                }
+
+    }
+
+
+};
+
+
+    httpRequest.open('GET', 'https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=boolean');
+    httpRequest.responseType = 'json';
+    httpRequest.send();
+
+});
