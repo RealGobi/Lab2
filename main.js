f621bcd6b164df4a2a6bca26a99e1f798e9433be
Jimmy Svensson <jimmy888swe@gmail.com>
2018-10-24 14:40:59 +0200

    sista sparning med foreach, go om till if loop ->

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ httpRequest.onreadystatechange = function() {
              // här hämtar vi och skriver ut frågorna ut, skapar html div mm
 
                  res.results.forEach(function(result, index ) {
+
+                  var correctAnswer1 = res.results[0].correct_answer;
+                  var correctAnswer2 = res.results[1].correct_answer;
+                  console.log(correctAnswer1,"\n",correctAnswer2);
                  var info = document.createElement('div');
                  info.classList.add('info');
                  info.id = index;
@@ -24,29 +28,42 @@ httpRequest.onreadystatechange = function() {
                  var infoContent = ` 
                  <div class="container">
                   <p> ${result.question} </p>
-                  <p class="correctA"> ${result.correct_answer} </p>
-                  <button type ="button" class="btnAll" id="btnTrue">True</button>
-                  <button type ="button" class="btnAll" id="btnFalse">False</button>
-                  </div> `;
+                  <p> ${result.correct_answer} </p>
+                  <button input type ="button" class="btnAll" id="btnTrue">True</button>
+                  <button input type ="button" class="btnAll" id="btnFalse">False</button>
+                  </div> 
+                  `;
                 
-                    // ersätter html med de vi hämtade oven
 
+                  // kopplar ihop knapparna och lagrar input
+                 var btnTrue = document.getElementById('btnTrue');
+                 var btnFalse = document.getElementById('btnFalse');
+                 btnTrue = 'True';
+                 btnFalse = 'False';
+                 
+              content.addEventListener('click', function(event){
+                  if (event.target.nodeName == 'BUTTON') {
+                      
+                      if (btnTrue == correctAnswer1){
+                          event.target.classList.add('trueC');
+                        
+                      }
+                      else if (btnFalse == correctAnswer1){
+                          event.target.classList.add('falseC');
+                         
+                      }
+                     
+                  }
+       
+              });
+                
+
+                    // ersätter html med de vi hämtade oven
+// skriver ut svaren -    <p class="correctA"> ${result.correct_answer} </p>
                 document.getElementById(info.id).innerHTML = infoContent; 
              });
 
 
-                var correctAnswer = res.results[1].correct_answer;
-                    console.log(correctAnswer);
-
-                    // kopplar ihop knapparna och lagrar input
-
-                content.addEventListener('click', function(event){
-                    if (event.target.nodeName == 'BUTTON') {
-                
-                        event.target.classList.add('true');
-                    }
-         
-                });
              // felmedelande från XMLHttpRequest
         } else  {
                 console.log('Något är fel, gör om, gör rätt!');
